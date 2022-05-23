88067ab1c03eb8ede30b014465a28be53b5f5b24
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-05-23 12:55:15 -0300

    fix solution 1047.js

--- Iniciante/URI 1047.js
+++ Iniciante/URI 1047.js
@@ -1,22 +1,37 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
 
-var line = lines.shift().split(" ");
-var hInicio = parseInt(line[0]);
-var mInicio = parseInt(line[1]);
-var hFim = parseInt(line[2]);
-var mFim = parseInt(line[3]);
-var totalH = hFim - hInicio;
-var totalM = mFim - mInicio;
+let line = lines.shift().split(" ");
 
-if (totalH < 0) {
-    totalH = 24 + (hFim - hInicio);
+let hoursInitial = parseInt(line[0]);
+let minuteInitial = parseInt(line[1]);
+let hoursFinal = parseInt(line[2]);
+let minuteFinal = parseInt(line[3]);
+
+let minutes = 0;
+let hours = 0;
+
+if (
+  hoursInitial == hoursFinal &&
+  minuteInitial == minuteFinal &&
+  hoursInitial == minuteFinal
+) {
+  console.log("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+} else {
+  if (hoursInitial > hoursFinal) {
+    hours = 24 - hoursInitial + hoursFinal;
+  } else if (hoursInitial < hoursFinal) {
+    hours = 24 - (24 - hoursFinal + hoursInitial);
+  }
+  if (minuteInitial > minuteFinal) {
+    minutes = 60 + minuteFinal - minuteInitial;
+    if (hoursInitial == hoursFinal) {
+      hours = 24 + hours - 1;
+    } else {
+      hours = hours - 1;
+    }
+  } else {
+    minutes = minuteFinal - minuteInitial;
+  }
+  console.log(`O JOGO DUROU ${hours} HORA(S) E ${minutes} MINUTO(S)`);
 }
-
-if (totalM < 0) {
-    totalM = 60 + (mFim - mInicio);
-    totalH--;
-}
-
-if (hInicio == hFim && mInicio == mFim) console.log('O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)');
-else console.log('O JOGO DUROU '+ totalH + ' HORA(S) E ' + totalM + ' MINUTO(S)');
