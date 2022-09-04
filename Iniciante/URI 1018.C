90dfb050655d0e599669881b8ab7bebb13c86bb2
Carlos Eduardo Amorim <86995782+caduamorimm-dev@users.noreply.github.com>
2022-09-03 23:27:10 -0300

    Inserido o problema 1018 em C

---
+++ Iniciante/URI 1018.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+
+      double raio, area, n;
+
+      scanf("%lf", &raio);
+
+      n = 3.14159;
+      area = (raio * raio) * n;
+
+      printf("A=%.4f\n", area);
+
+
+    return 0;
+}
