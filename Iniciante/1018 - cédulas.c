9e8e01ba53358d3ba67800531cdc65e6a5c818a8
Carlos Eduardo Amorim <86995782+caduamorimm-dev@users.noreply.github.com>
2022-09-09 19:08:53 -0300

    Correção 1018

--- Iniciante/1018 - cédulas.c
+++ Iniciante/1018 - cédulas.c
@@ -1,64 +1,38 @@
 #include <stdio.h>
-#include <math.h>
+ 
+int main() {
+        
+        int notasInseridas, atual;
+        int notas1, notas2, notas5, notas10;
+        int notas20, notas50, notas100;
+        
+        scanf("%d", &notasInseridas);
+        scanf("%d%d%d%d", &notas1, &notas2, &notas5, &notas10);
+        scanf("%d%d%d", &notas20, &notas50, &notas100);
+        
+        atual = notasInseridas;
+        notas100 = atual / 100;
+        atual -= notas100 * 100;
+        notas50 = atual / 50;
+        atual -= notas50 * 50;
+        notas20 = atual / 20;
+        atual -= notas20 * 20;
+        notas10 = atual / 10;
+        atual -= notas10 * 10;
+        notas5 = atual / 5;
+        atual -= notas5 * 5;
+        notas2 = atual / 2;
+        atual -= notas2 * 2;
+        notas1 = atual;
+        
+        printf("%d\n", notasInseridas);
+        printf("%d nota(s) de R$ 100,00\n", notas100);
+        printf("%d nota(s) de R$ 50,00\n", notas50);
+        printf("%d nota(s) de R$ 20,00\n", notas20);
+        printf("%d nota(s) de R$ 10,00\n", notas10);
+        printf("%d nota(s) de R$ 5,00\n", notas5 );
+        printf("%d nota(s) de R$ 2,00\n", notas2 );
+        printf("%d nota(s) de R$ 1,00\n", notas1 );
 
-int main (){
-
-  int a, b, c;
-
-  scanf("%d %d %d", &a, &b, &c);
-
-  if (a <= b && b <= c){
-    printf("%d\n", a);
-    printf("%d\n", b);
-    printf("%d\n", c);
-  }
-  else{
-
-    if (a <= c && c <= b){
-      printf("%d\n", a);
-      printf("%d\n", c);
-      printf("%d\n", b);
-    }
-    else{
-
-      if (b <= a && a <= c){
-        printf("%d\n", b);
-        printf("%d\n", a);
-        printf("%d\n", c);
-      }
-      else{
-
-        if (b <= c && c <= a){
-          printf("%d\n", b);
-          printf("%d\n", c);
-          printf("%d\n", a);
-        }
-        else{
-
-          if (c <= a && a <= b){
-            printf("%d\n", c);
-            printf("%d\n", a);
-            printf("%d\n", b);
-          }
-          else{
-            if (a == b && b == c){
-              printf("%d\n", a);
-              printf("%d\n", b);
-              printf("%d\n", c);
-            }
-            else{
-              printf("%d\n", c);
-              printf("%d\n", b);
-              printf("%d\n", a);
-            }
-          }
-        }
-      }
-    }
-
-}
-  printf("\n");
-  printf("%d\n", a);
-  printf("%d\n", b);
-  printf("%d\n", c);
+    return 0;
 }
